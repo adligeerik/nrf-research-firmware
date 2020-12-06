b11fa68181f622033cbd630bb0fa49b32957062b
David <david.rozenbeek@gmail.com>
2020-12-06 19:17:00 +0100

    implement keyboard press in mousetx

--- tools/nrf24-mousetx.py
+++ tools/nrf24-mousetx.py
@@ -16,86 +16,165 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-
-import time, logging
 from lib import common
+from pynput.keyboard import Listener, Key
+import time, logging
 
-# Parse command line arguments and initialize the radio
-common.init_args('./nrf24-sniffer.py')
-common.parser.add_argument('-a', '--address', type=str, help='Address to sniff, following as it changes channels', required=True)
-common.parser.add_argument('-t', '--timeout', type=float, help='Channel timeout, in milliseconds', default=100)
-common.parser.add_argument('-k', '--ack_timeout', type=int, help='ACK timeout in microseconds, accepts [250,4000], step 250', default=250)
-common.parser.add_argument('-r', '--retries', type=int, help='Auto retry limit, accepts [0,15]', default=1, choices=xrange(0, 16), metavar='RETRIES')
-common.parser.add_argument('-p', '--ping_payload', type=str, help='Ping payload, ex 0F:0F:0F:0F', default='0F:0F:0F:0F', metavar='PING_PAYLOAD')
-common.parse_and_init()
+MAX_12B = 2047
+MIN_12B = -2048
+step = 1
+running = True
+x = 0
+y = 0
 
-# Parse the address
-address = common.args.address.replace(':', '').decode('hex')[::-1][:5]
-address_string = ':'.join('{:02X}'.format(ord(b)) for b in address[::-1])
-if len(address) < 2:
-  raise Exception('Invalid address: {0}'.format(common.args.address))
+def on_press(key):  # The function that's called when a key is pressed
+    global x, y, MAX_12B, MIN_12B, step
 
-# Put the radio in sniffer mode (ESB w/o auto ACKs)
-common.radio.enter_sniffer_mode(address)
+    if step < 50:
+        step += 1
 
-# Convert channel timeout from milliseconds to seconds
-timeout = float(common.args.timeout) / float(1000)
-print('Payload')
-print(common.args.ping_payload)
+    if key == Key.up:
+        y += step
+        if y > MAX_12B:
+            y = MAX_12B
+        print('Up key pressed: ' + str(y))
 
-# Parse the ping payload
-ping_payload = common.args.ping_payload.replace(':', '').decode('hex')
+    elif key == Key.down:
+        y -= step
+        if y < MIN_12B:
+            y = MIN_12B
+        print('Down key pressed: '+ str(y))
+    
+    elif key == Key.left:
+        x -= step
+        if x < MIN_12B:
+            x = MIN_12B
+        print('Left key pressed: ' + str(x))
+    
+    elif key == Key.right:
+        x += step
+        if x > MAX_12B:
+            x = MAX_12B
+        print('Right key pressed: ' + str(x))
 
-# Format the ACK timeout and auto retry values
-ack_timeout = int(common.args.ack_timeout / 250) - 1
-ack_timeout = max(0, min(ack_timeout, 15))
-retries = max(0, min(common.args.retries, 15))
+def on_release(key):  # The function that's called when a key is released
+    global x,y,running,step 
+    
+    step = 1
+    if key == Key.up:
+        y = 0
+        #print('Up key de-pressed: ' + str(y))
+    elif key == Key.down:
+        y = 0
+        #print('Down key de-pressed: ' + str(y))
+    elif key == Key.left:
+        x = 0
+        #print('Left key de-pressed: ' + str(x))
+    elif key == Key.right:
+        x = 0
+        #print('Right key de-pressed: ' + str(x))
+    elif key == Key.esc:
+        print("Esc key de-pressed")
+        running = False
 
-# Sweep through the channels and decode ESB packets in pseudo-promiscuous mode
-last_ping = time.time()
-channel_index = 0
-while True:
+def main():
+  global x,y,running
 
-  # Follow the target device if it changes channels
-  if time.time() - last_ping > timeout:
+  # Start keyboard listener
+  listener = Listener(
+        on_press=on_press,
+        on_release=on_release)
+  
+  listener.start()
+  # Keep alive payload 00:40:01:18:A7
+  # Parse command line arguments and initialize the radio
+  common.init_args('./nrf24-sniffer.py')
+  common.parser.add_argument('-a', '--address', type=str, help='Address to sniff, following as it changes channels', required=True)
+  common.parser.add_argument('-t', '--timeout', type=float, help='Channel timeout, in milliseconds', default=100)
+  common.parser.add_argument('-k', '--ack_timeout', type=int, help='ACK timeout in microseconds, accepts [250,4000], step 250', default=250)
+  common.parser.add_argument('-r', '--retries', type=int, help='Auto retry limit, accepts [0,15]', default=1, choices=xrange(0, 16), metavar='RETRIES')
+  common.parser.add_argument('-p', '--ping_payload', type=str, help='Ping payload, ex 0F:0F:0F:0F', default='00:40:01:18:A7', metavar='PING_PAYLOAD')
+  common.parse_and_init()
 
-    # First try pinging on the active channel
-    if not common.radio.transmit_payload(ping_payload, ack_timeout, retries):
+  # Parse the address
+  address = common.args.address.replace(':', '').decode('hex')[::-1][:5]
+  address_string = ':'.join('{:02X}'.format(ord(b)) for b in address[::-1])
+  if len(address) < 2:
+    raise Exception('Invalid address: {0}'.format(common.args.address))
 
-      # Ping failed on the active channel, so sweep through all available channels
-      success = False
-      for channel_index in range(len(common.channels)):
-        common.radio.set_channel(common.channels[channel_index])
-        if common.radio.transmit_payload(ping_payload, ack_timeout, retries):
+  # Put the radio in sniffer mode (ESB w/o auto ACKs)
+  common.radio.enter_sniffer_mode(address)
 
-          # Ping successful, exit out of the ping sweep
-          last_ping = time.time()
-          logging.debug('Ping success on channel {0}'.format(common.channels[channel_index]))
-          success = True
-          break
+  # Convert channel timeout from milliseconds to seconds
+  timeout = float(common.args.timeout) / float(1000)
+  print('Payload')
+  print(common.args.ping_payload)
 
-      # Ping sweep failed
-      if not success: logging.debug('Unable to ping {0}'.format(address_string))
+  # Parse the ping payload
+  ping_payload = common.args.ping_payload.replace(':', '').decode('hex')
 
-    # Ping succeeded on the active channel
-    else:
-      logging.debug('Ping success on channel {0}'.format(common.channels[channel_index]))
+  # Format the ACK timeout and auto retry values
+  ack_timeout = int(common.args.ack_timeout / 250) - 1
+  ack_timeout = max(0, min(ack_timeout, 15))
+  retries = max(0, min(common.args.retries, 15))
+
+  # Sweep through the channels and decode ESB packets in pseudo-promiscuous mode
+  last_ping = time.time()
+  channel_index = 0
+  while running:
+
+    # Follow the target device if it changes channels
+    if time.time() - last_ping > timeout:
+
+      # First try pinging on the active channel
+      if not common.radio.transmit_payload(ping_payload, ack_timeout, retries):
+
+        # Ping failed on the active channel, so sweep through all available channels
+        success = False
+        for channel_index in range(len(common.channels)):
+          common.radio.set_channel(common.channels[channel_index])
+          if common.radio.transmit_payload(ping_payload, ack_timeout, retries):
+
+            # Ping successful, exit out of the ping sweep
+            last_ping = time.time()
+            logging.debug('Ping success on channel {0}'.format(common.channels[channel_index]))
+            success = True
+            break
+
+        # Ping sweep failed
+        if not success: logging.debug('Unable to ping {0}'.format(address_string))
+
+      # Ping succeeded on the active channel
+      else:
+        logging.debug('Ping success on channel {0}'.format(common.channels[channel_index]))
+        last_ping = time.time()
+
+    # Try to send mouse packets if arrow keys has been pressed
+    if x != 0 or y != 0:
+      mouse_payload = '00:C2:00:00:02:20:FD:00:00:1F'.replace(':', '').decode('hex')
+      common.radio.transmit_payload(mouse_payload, ack_timeout, retries)
+      
+
+    # Receive payloads
+    value = common.radio.receive_payload()
+    if value[0] == 0:
+
+      # Reset the channel timer
       last_ping = time.time()
 
-  # Receive payloads
-  value = common.radio.receive_payload()
-  if value[0] == 0:
+      # Split the payload from the status byte
+      payload = value[1:]
 
-    # Reset the channel timer
-    last_ping = time.time()
+      # Log the packet
+      logging.info('{0: >2}  {1: >2}  {2}  {3}'.format(
+                common.channels[channel_index],
+                len(payload),
+                address_string,
+                ':'.join('{:02X}'.format(b) for b in payload)))
+    
+    # End of main loop
+  
+  listener.stop()
 
-    # Split the payload from the status byte
-    payload = value[1:]
-
-    # Log the packet
-    logging.info('{0: >2}  {1: >2}  {2}  {3}'.format(
-              common.channels[channel_index],
-              len(payload),
-              address_string,
-              ':'.join('{:02X}'.format(b) for b in payload)))
-
+if __name__ == '__main__':
+    main()
